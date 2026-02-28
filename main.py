5060801c86bcd1a59a517aec1cced7ba3d732a01
varshacodes-sys <shavar1230@gmail.com>
2026-02-28 11:54:35 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello, GitHub!")
+from tasks import add_task, view_tasks
+
+while True:
+    print("\nStudent Planner")
+    print("1. Add Task")
+    print("2. View Tasks")
+    print("3. Exit")
+
+    choice = input("Choose: ")
+
+    if choice == "1":
+        add_task()
+    elif choice == "2":
+        view_tasks()
+    elif choice == "3":
+        break
