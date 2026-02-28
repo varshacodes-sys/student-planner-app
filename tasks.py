471a05e715ff1a56034e5b50648948f2833fb506
varshacodes-sys <shavar1230@gmail.com>
2026-02-28 11:54:51 -0600

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,1 +1,11 @@
-print("Hello, GitHub!")
+tasks = []
+
+def add_task():
+    name = input("Task name: ")
+    due = input("Due date: ")
+    tasks.append({"name": name, "due": due})
+    print("Task added!")
+
+def view_tasks():
+    for t in tasks:
+        print(f"{t['name']} - Due: {t['due']}")
